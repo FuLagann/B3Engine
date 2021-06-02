a6fd660824da517c933f63fe7ed4ac533fcdda04
Paul G.B <fulagann@gmail.com>
2021-06-01 20:04:43 -0700

    Cleaning up: Ray tests

--- test/Base/Math/Ray.Test.cs
+++ test/Base/Math/Ray.Test.cs
@@ -2,42 +2,88 @@
 using Xunit;
 
 namespace B3.Testing {
+	/// <summary>Tests the <see cref="B3.Ray"/> structure to make sure it works correctly. Contains 7 tests</summary>
 	public class RayTest {
-		// [Fact]
-		// public void Constructors() {
-		// 	Assert.Equal(new Ray(Vector3.Zero, Vector3.Zero), Ray.Empty);
-		// 	Assert.Equal(new Ray(Vector3.Zero, Vector3.UnitX), Ray.UnitXDir);
-		// 	Assert.Equal(new Ray(Vector3.Zero, Vector3.UnitY), Ray.UnitYDir);
-		// 	Assert.Equal(new Ray(Vector3.Zero, Vector3.UnitZ), Ray.UnitZDir);
-		// }
+		#region Public Test Methods
 		
-		// [Theory]
-		// [InlineData(2, 0, 0, 0, 1, 0, 0, 2, 0, 0)]
-		// [InlineData(0.57735026919f, 0, 0, 0, 1, 1, 1, 0.3333333, 0.3333333, 0.3333333)]
-		// [InlineData(-5, 0, 0, 0, 0, 1, 0, 0, -5, 0)]
-		// public void GetPoint(float dist, float x, float y, float z, float dx, float dy, float dz, float ex, float ey, float ez) {
-		// 	// Variables
-		// 	Ray ray = new Ray(new Vector3(x, y, z), new Vector3(dx, dy, dz));
-		// 	Vector3 e = new Vector3(ex, ey, ez);
+		/// <summary>
+		/// Tests the <see cref="B3.Ray.Direction"/> functionality.
+		/// Creates a ray and checks to see if the direction is correct (normalized)
+		/// </summary>
+		[Fact]
+		public void Constructor_Direction_ReturnsNormalizedVector3() {
+			// Variables
+			Ray ray = new Ray(Vector3.One, Vector3.One);
+			Vector3 expected = Vector3.UnitOne;
 			
-		// 	Assert.Equal(e, Ray.GetPoint(dist, ref ray));
-		// }
+			Assert.Equal(expected, ray.Direction);
+		}
 		
-		// [Theory]
-		// [InlineData(1, 0, 0, 0, 0, 0, 1, 1, 0, 0.5, 0.5, 0)]
-		// [InlineData(0, 0, 0, 1, 1, 1, 1, -1, 1, 0.6666666, 1.3333334, 0.6666666)]
-		// public void GetPointFromPoint(
-		// 	float ax, float ay, float az,
-		// 	float x, float y, float z,
-		// 	float dx, float dy, float dz,
-		// 	float ex, float ey, float ez
-		// ) {
-		// 	// Variables
-		// 	Vector3 a = new Vector3(ax, ay, az);
-		// 	Vector3 e = new Vector3(ex, ey, ez);
-		// 	Ray ray = new Ray(new Vector3(x, y, z), new Vector3(dx, dy, dz));
+		/// <summary>
+		/// Tests the <see cref="B3.Ray.Origin"/> functionality.
+		/// Creates a ray and checks to see if the origin is correct (not normalized)
+		/// </summary>
+		[Fact]
+		public void Constructor_Origin_ReturnsVector3() {
+			// Variables
+			Ray ray = new Ray(Vector3.One, Vector3.One);
+			Vector3 expected = Vector3.One;
 			
-		// 	Assert.Equal(e, Ray.GetPointFromPoint(ref a, ref ray));
-		// }
+			Assert.Equal(expected, ray.Origin);
+		}
+		
+		/// <summary>
+		/// Tests the <see cref="B3.Ray.GetPoint(float, Ray)"/> functionality.
+		/// Using a ray and a distance, gets a point along the ray and checks to see if its correct
+		/// </summary>
+		[Theory]
+		[InlineData(2, 0, 0, 0, 1, 0, 0, 2, 0, 0)]
+		[InlineData(0.57735026919f, 0, 0, 0, 1, 1, 1, 0.3333333, 0.3333333, 0.3333333)]
+		[InlineData(-5, 0, 0, 0, 0, 1, 0, 0, -5, 0)]
+		public void GetPoint_Distance_ReturnsVector3(
+			float distance, float x, float y, float z,
+			float dx, float dy, float dz,
+			float ex, float ey, float ez
+		) {
+			// Variables
+			Vector3 origin = new Vector3(x, y, z);
+			Vector3 direction = new Vector3(dx, dy, dz);
+			Ray ray = new Ray(origin, direction);
+			Vector3 expected = new Vector3(ex, ey, ez);
+			Vector3 actual = Ray.GetPoint(distance, ref ray);
+			// TODO: Add ray.GetPoint(distance) method
+			// TODO: Add distance * ray operator
+			
+			Assert.Equal(expected, actual);
+		}
+		
+		/// <summary>
+		/// Tests the <see cref="B3.Ray.Direction"/> functionality.
+		/// Using a ray and a point, gets the closest point along the ray from the given point and checks to see if its correct
+		/// </summary>
+		[Theory]
+		[InlineData(1, 0, 0, 0, 0, 0, 1, 1, 0, 0.5, 0.5, 0)]
+		[InlineData(0, 0, 0, 1, 1, 1, 1, -1, 1, 0.6666666, 1.3333334, 0.6666666)]
+		public void GetPointFromPoint(
+			float ax, float ay, float az,
+			float x, float y, float z,
+			float dx, float dy, float dz,
+			float ex, float ey, float ez
+		) {
+			// Variables
+			Vector3 origin = new Vector3(x, y, z);
+			Vector3 direction = new Vector3(dx, dy, dz);
+			Ray ray = new Ray(origin, direction);
+			Vector3 actual = new Vector3(ax, ay, az);
+			Vector3 expected = new Vector3(ex, ey, ez);
+			// TODO: Add ray.GetPointFromPoint(point) method
+			// TODO: Add point ~ ray method, if possible
+			
+			Ray.GetPointFromPoint(ref actual, ref ray, out actual);
+			
+			Assert.Equal(expected, actual);
+		}
+		
+		#endregion // Public Test Methods
 	}
 }
